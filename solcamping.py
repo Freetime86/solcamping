1f2468ad3de8e639f12c6d67713430a72303e0cc
psm07 <psm0705@gmail.com>
2023-05-22 12:21:00 +0900

    보완

--- solcamping.py
+++ solcamping.py
@@ -82,8 +82,8 @@ def main(thread_name):
                 month = date[0:2]
                 day = date[2:4]
                 if sel_month == month:
-                    date_str_begin = datetime.now().strftime("%Y-%m-%d") + ' 09:59:59'
-                    date_str_end = datetime.now().strftime("%Y-%m-%d") + ' 10:00:05'
+                    date_str_begin = datetime.now().strftime("%Y-%m-%d") + ' 10:00:00'
+                    date_str_end = datetime.now().strftime("%Y-%m-%d") + ' 10:00:10'
 
                     date_dt_begin = datetime.strptime(date_str_begin, '%Y-%m-%d %H:%M:%S')
                     date_dt_end = datetime.strptime(date_str_end, '%Y-%m-%d %H:%M:%S')
