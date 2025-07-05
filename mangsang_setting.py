d5225d6b1282dc6e2a7c6894b0028c715100da71
Sangmin <psm0705@gmail.com>
2025-07-05 12:27:12 +0900

    Changes

---
+++ mangsang_setting.py
@@ -0,0 +1,17 @@
+class config:
+
+    def dataset(self):
+        dict_data = {
+            #TARGET SETTING
+            'ROOM_WANT': [],
+            'ROOM_PICK': [],
+            
+            # 1.든바다, 2.난바다, 3.허허바다, 4.전통한옥, 5.캐라반, 6.자동차야영장, 7.글램핑(2), 8.글램핑(4), 9.캐빈하우스
+            'SITE_TYPE': '3',
+            
+            #TIME SETTING
+            'SELYEARLIST': ['2025'],
+            'SELMONTHLIST': ['08'],
+            'SELDAYLIST': ['04'],
+            'PERIOD': 3
+        }
