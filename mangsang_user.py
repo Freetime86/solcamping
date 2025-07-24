b4263706d774c1dcb5d56d6767cdb0b8cda8dcae
Sangmin <psm0705@gmail.com>
2025-07-25 08:22:51 +0900

    Changes

--- mangsang_user.py
+++ mangsang_user.py
@@ -65,11 +65,11 @@ def _list():
                'rphone': '01041798796',
                'area_code': '1006'
                },
-        '10': {'user_name': '',
-               'rpwd': '',
-               'rid': '',
-               'rphone': '',
-               'area_code': ''
+        '10': {'user_name': '홍석환',
+               'rpwd': 'hwan1150^^',
+               'rid': 'hong5870',
+               'rphone': '01082853000',
+               'area_code': '1001'
                },
         '11': {'user_name': '김은경',
                'rpwd': 'rladmsrud00',
